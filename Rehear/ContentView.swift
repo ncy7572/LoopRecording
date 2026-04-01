c6e81e8444c4faed202e95646e1b3cc5b2f19d8c
CyNi <ncy7572@gmail.com>
2026-04-01 13:56:28 +0800

    Initial Commit

---
+++ Rehear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rehear
+//
+//  Created by 倪晨一 on 2026/4/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rehear/RehearApp.swift
@@ -0,0 +1,17 @@
+//
+//  RehearApp.swift
+//  Rehear
+//
+//  Created by 倪晨一 on 2026/4/1.
+//
+
+import SwiftUI
+
+@main
+struct RehearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
